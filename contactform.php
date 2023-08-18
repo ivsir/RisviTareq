d573b9d570e0bac94bb40f1658a6bfa7eecd0f4b
Risvi-Tareq <ivsirqerat@gmail.com>
2023-08-17 23:52:51 -0700

    fixed PHP form

--- contactform.php
+++ contactform.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST['submit'])){
     $name = $_POST['name'];
-    $mailFrom = $_POST['mailFrom'];
+    $mailFrom = $_POST['email'];
     $message = $_POST['message'];
 
     $mailTo = "ivsirqerat@gmail.com";
