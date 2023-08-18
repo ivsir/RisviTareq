9da086fd24ac7a544134131a6b77aa32aee0cb77
Risvi-Tareq <ivsirqerat@gmail.com>
2023-08-17 18:20:14 -0700

    setup the PHP

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html><!--  This site was created in Webflow. https://www.webflow.com  -->
+<!--  Last Published: Sat Aug 12 2023 06:52:50 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="64c8249b4207539022be7290" data-wf-site="64b6d6c34338291a9394adeb">
+
+<head>
+  <meta charset="utf-8">
+  <title>ivsir portfolio</title>
+  <meta content="ivsir portfolio" property="og:title">
+  <meta content="ivsir portfolio" property="twitter:title">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/social-media-platform.webflow.css" rel="stylesheet" type="text/css">
+  <script
+    type="text/javascript">!function (o, c) { var n = c.documentElement, t = " w-mod-"; n.className += t + "js", ("ontouchstart" in o || o.DocumentTouch && c instanceof DocumentTouch) && (n.className += t + "touch") }(window, document);</script>
+  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.png" rel="apple-touch-icon">
+</head>
+
+<body class="body-2">
+  <div id="navbar" class="navbar">
+    <div class="main-nav">
+      <div data-animation="default" data-collapse="all" data-duration="400" data-easing="ease" data-easing2="ease"
+        role="banner" class="navbar-2 w-nav">
+        <div class="div-block-10">
+          <a href="#intro" class="w-nav-brand"><img src="images/ivsir-logo.png" loading="lazy" alt=""
+              class="image-3"></a>
+          <nav role="navigation" class="nav-menu w-nav-menu">
+            <a href="#intro" class="nav-link-2 w-nav-link">Home</a>
+            <a href="#projects" class="nav-link-3 w-nav-link">Projects</a>
+            <a href="#contact-section" class="nav-link-4 w-nav-link">Contact</a>
+          </nav>
+          <div class="menu-button-2 w-nav-button">
+            <div class="icon w-icon-nav-menu"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="main-content">
+    <div id="intro" class="intro-section">
+      <div class="headliner">
+        <div class="title">I&#x27;m <span class="text-span-2">Risvi Tareq</span>,
+          <br>a <span class="text-span">
+            <a href="https://github.com/ivsir " target="_blank" class="text-span" style="text-decoration: none;">
+            Developer
+            </a>
+          </span> by day,<br><span class="text-span-3">
+            <a href="https://beatstars.com/ivsir" target="_blank" class="text-span-3" style="text-decoration: none;">
+            Sound Engineer
+            </a>
+          </span> by night<span class="blinking">_</span>
+        </div>
+      </div>
+    </div>
+    <div id="projects" class="divider"></div>
+    <div class="projects-section">
+      <div class="subheader">Projects</div>
+      <div class="projects">
+        <div id="w-node-_6558b361-fa89-7218-c191-fc64fcf794f8-22be7290" class="project1">
+          <a href="https://code-collab-dallas.herokuapp.com/" target="_blank" class="project-link w-inline-block"><img
+              src="images/code-collab-logo.png" loading="lazy" alt="" class="image-5"></a>
+          <div class="div-block-6">
+            <div class="project-title">CodeCollab</div>
+            <div class="text-block-3">MongoDB • GraphQL • React • NodeJS • Express</div>
+          </div>
+        </div>
+        <div id="w-node-aa7d98ac-f6a0-9f13-7675-e85703992128-22be7290" class="project2">
+          <a href="https://ivsir.github.io/Sanrio-Email-Template/" target="_blank" class="project-link w-inline-block"><img src="images/sanrio.png" loading="lazy" alt=""
+              class="image-5"></a>
+          <div class="div-block-6">
+            <div class="project-title">Sanrio Email Marketing</div>
+            <div class="text-block-3">HTML • CSS</div>
+          </div>
+        </div>
+        <div id="w-node-a937fff9-0594-f795-093d-0a02bee98c60-22be7290" target="_blank" class="project3">
+          <a href="https://dev.clearbluesea.org/" class="project-link w-inline-block"><img src="images/cbs.png" loading="lazy" alt=""
+              class="image-5"></a>
+          <div class="div-block-6">
+            <div class="project-title">Clear Blue Sea</div>
+            <div class="text-block-3">Wordpress • Photoshop</div>
+          </div>
+        </div>
+        <div id="w-node-_835d2085-e4fc-ca1e-d4c0-550b889509f6-22be7290" target="_blank"Yy class="project4">
+          <a href="https://bibliomania.herokuapp.com/" class="project-link w-inline-block"><img src="images/bibliomania.png" loading="lazy" alt=""
+              class="image-5"></a>
+          <div class="div-block-6">
+            <div class="project-title">Bibliomania</div>
+            <div class="text-block-3">Handlebars • Third-Party-API • JavaScript</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="contact-section" class="divider"></div>
+    <div class="contact-section">
+      <div class="subheader">Contact</div>
+      <div id="section3">
+        <a id="contact" class="js--wp-3" name="contact"></a>
+        <div class="row">
+          <div class="form-wrapper">
+            <div class="form-inner-wrapper">
+              <form  action="contactform.php" method="POST" enctype="text/plain" id="contactForm" >
+                <div class="field-list clear">
+                  <div id="name" class="form-item field email required">
+                    <label class="form-title" for="name">Name <span class="required">*</span></label>
+                    <input class="field-element" name="name" x-autocompletetype="name" type="text" spellcheck="false">
+                  </div>
+                  <div id="email" class="form-item field email required">
+                    <label class="form-title" for="email">Email Address <span class="required">*</span></label>
+                    <input class="field-element" name="email" x-autocompletetype="email" type="text" id="email-field">
+                  </div>
+                  <div id="textarea" class="form-item field textarea required">
+                    <label class="form-title" for="textarea-field">Message <span class="required">*</span></label>
+                    <textarea class="field-element" id="textarea--field" name="message"></textarea>
+                  </div>
+                  <div class="form-button-wrapper form-button-wrapper--align-center">
+                    <input class="button" type="submit" name="submit" value="SUBMIT" >
+                  </div>
+
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=64b6d6c34338291a9394adeb"
+    type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+    crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-1b93190375e9ccc259df3a57c1abc0e64599724ae30d7ea4c6877eb615f89387.js"></script>
+</body>
+
+</html>
